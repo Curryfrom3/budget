239499f78c495b1ccc3a50b49d819ec2da65a8ba
Deon Curry <deoncvn68@gmail.com>
2025-02-27 16:37:19 -0500

    deleted code that was

--- server.js
+++ server.js
@@ -15,25 +15,3 @@ const MONGO_URI = process.env.MONGO_URI;
 // Connect to MongoDB
 
 // Middleware
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(methodOverride('_method'));
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: MONGO_URI }),
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1-day session
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-// Routes
-app.use('/', require('./routes/indexRoutes'));
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/transactions', require('./routes/transactionRoutes'));
-
-// Start Server
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
